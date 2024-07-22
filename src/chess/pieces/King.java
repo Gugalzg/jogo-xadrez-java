86dc32a17b0976098918bbf8ad2c7ea8340e8d45
Guga <luizgustavoguga15@gmail.com>
2024-07-22 13:45:13 -0300

    Possíveis movimentos para as peças

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -13,4 +13,9 @@ import chess.Color;
 		public String toString() {
 			return "K";
 		}
+		@Override
+		public boolean[][] possibleMoves() {
+			boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+			return mat;
+		}
 }
